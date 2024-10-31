d1de9a55c8c49ce51f7e95803ce73f3f15f5370b
KoviDom <dominik.kovac01@seznam.cz>
2024-10-31 11:55:29 +0100

    m

--- src/ExchangeRateList.jsx
+++ src/ExchangeRateList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { fetchExchangeRates } from './utils/api'; // Načti funkci pro API request
+import { fetchExchangeRates } from './utils/api'; // funkce pro API request
 
 const ExchangeRateList = () => {
 
